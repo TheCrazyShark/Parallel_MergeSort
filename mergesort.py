c710a2a33eadd352d816fd113bfa1fc3bf02a7c8
jgearhart13 <79217798+jgearhart13@users.noreply.github.com>
2022-04-05 21:18:05 -0400

    Update mergesort.py
    Changed it back to Process gonna fuck around with queue see if i can get it to work, should be more efficent I believe.

--- mergesort.py
+++ mergesort.py
@@ -1,6 +1,6 @@
 from time import perf_counter
 import multiprocessing as mp
-def logFile(time1,time2):
+def logFile(time1,time2): #Used to write to a file named merge.log
     f=open("merge.log","a")
     data = str(time1-time2)
     f.write("Elapsed time: "+ data)
@@ -11,12 +11,12 @@ def startTime():
 def endTime():
     end = perf_counter()
     return end 
-def mergeSort(arr):
+def mergeSort(arr): #takes in an array and performs mergesorts
     if len(arr) > 1:
         mid = len(arr)//2 #finding middle of array
-        L = arr[:mid]
+        L = arr[:mid] #splits left side of array
 
-        R = arr[mid:]
+        R = arr[mid:] #splits right side
         mergeSort(L)
         mergeSort(R)
         i = j = k = 0
@@ -39,21 +39,23 @@ def mergeSort(arr):
             k+=1
 def printList(arr):
     for i in range(len(arr)):
-         print(arr[i], end=" ")
+         print(arr[i], end=" ") #used for printing the array, just used for testing probably omit this function
     
     print()
 def dataInput():
-    print("Input data file", end="\n")
-#def logFile():
+    print("Input data file by name or location", end="\n")
+    fName = input()
+    return fName
+def readFile():
+    #this will depend on the structure of the file 
 if __name__ == '__main__':
     arr = [12,11,13,5,6,7]
     print("Given array is", end="\n")
     printList(arr)
-    proccess = mp.cpu_count()
-    p = mp.Pool(processes=process)
     start = startTime()
-    p.map(mergeSort(arr), range(1))
-    p.close()
+    p = mp.Process(target=mergeSort, args=arr)
+    p.start()
+    p.join()
     end = endTime()
     print("Sort finished in: ",end-start)
     print("Sorted array is: ", end="\n")
