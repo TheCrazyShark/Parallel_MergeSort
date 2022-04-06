d4e3775284007a098474aef1881bf6abff016ccf
Shane Smith <sas1041@sru.edu>
2022-04-06 14:56:23 -0400

    Made it so the default merge sort runs by commenting out the mp & pool lines. Also added some formatting to the output and code

--- mergesort.py
+++ mergesort.py
@@ -1,16 +1,20 @@
 from time import perf_counter
 import multiprocessing as mp
+
 def logFile(time1,time2): #Used to write to a file named merge.log
     f=open("merge.log","a")
     data = str(time1-time2)
-    f.write("Elapsed time: "+ data)
+    f.write("Elapsed time: "+ data + "\n")
     f.close()
+
 def startTime():
     start= perf_counter()
     return start
+
 def endTime():
     end = perf_counter()
     return end 
+
 def mergeSort(arr): #takes in an array and performs mergesorts
     if len(arr) > 1:
         mid = len(arr)//2 #finding middle of array
@@ -37,25 +41,30 @@ def mergeSort(arr): #takes in an array and performs mergesorts
             arr[k] = R[j]
             j+=1
             k+=1
+
 def printList(arr):
     for i in range(len(arr)):
          print(arr[i], end=" ") #used for printing the array, just used for testing probably omit this function
     
     print()
+
 def dataInput():
     print("Input data file by name or location", end="\n")
     fName = input()
     return fName
-def readFile():
+
+#def readFile():
     #this will depend on the structure of the file 
+
 if __name__ == '__main__':
     arr = [12,11,13,5,6,7]
     print("Given array is", end="\n")
     printList(arr)
     start = startTime()
-    p = mp.Process(target=mergeSort, args=arr)
-    p.start()
-    p.join()
+    mergeSort(arr)
+    #p = mp.Process(target=mergeSort, args=arr)
+    #p.start()
+    #p.join()
     end = endTime()
     print("Sort finished in: ",end-start)
     print("Sorted array is: ", end="\n")
