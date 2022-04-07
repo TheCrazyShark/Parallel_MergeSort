aac2c1c0767d057ce02f8a34c91cce4fd31e6fe7
Shane Smith <sas1041@sru.edu>
2022-04-07 19:39:41 -0400

    Added repeated process and average time & speedup calculation.

--- mergesort.py
+++ mergesort.py
@@ -3,7 +3,7 @@ import random
 import multiprocessing as mp
 
 # Used to write to a file named merge.log
-def logFile(seqtime1,seqtime2,paratime1,paratime2): #Used to write to a file named merge.log
+def logFile(seqtime1,seqtime2,paratime1,paratime2, speedup): #Used to write to a file named merge.log
     f=open("merge.log","a")
     f.write("Sequential: \n")
     f.write("Elapsed time: ")
@@ -11,10 +11,8 @@ def logFile(seqtime1,seqtime2,paratime1,paratime2): #Used to write to a file nam
     f.write("Parallel: "+ "\n"+ "Elapsed time: ")
     f.write('{:.20f}'.format(paratime1-paratime2) +"\n")
     f.write("The Speed up of the program was: ")
-    speedup = (seqtime1-seqtime2) // (paratime1-paratime2)
-    f.write('{:.10f}'.format(speedup))
+    f.write('{:.10f}'.format(speedup) +"\n")
     f.close()
-
 
 # Start Timer
 def startTime():
@@ -54,7 +52,6 @@ def mergeSort(arr):
             j += 1
             k += 1
 
-
 # Generates a number of element using a gaussian distribution
 def generateNumbers(numElements):
     randomArray = []
@@ -72,35 +69,61 @@ def copyArray(arr):
         equalArray.insert(i, arr[i])
     return equalArray
 
+def sumArray(arr):
+    sum = 0
+
+    for i in arr:
+        sum += i
+    return sum
 
 if __name__ == '__main__':
     # Generate & Display Data Array
-    userIn = int(input("Enter the size of the array to be randomly generated ")) 
-    arr1 = generateNumbers(userIn)
-    arr2 = copyArray(arr1)
-    print("Given array is: ", arr1[:5])
+    userInArraySize = int(input("Enter the size of the array to be randomly generated: "))
+    userInRunTimes = int(input("Enter the number of times you would like to run with random data: "))
 
-    # Do Sequential Merge Sort
-    start = startTime()
-    mergeSort(arr1)
-    end = endTime()
+    # Run and store times 10 times to get average times
+    sTimeArr = []
+    pTimeArr = []
+    avgSTime = []
+    avgPTime = []
+    for x in range(userInRunTimes):
+        arr1 = generateNumbers(userInArraySize)
+        arr2 = copyArray(arr1)
+        print("Given array is: ", arr1[:5])
 
-    # Print Sequential Data
-    print("Sorted Array is: ", arr1[:5])
-    print("Sequential Merge Sort Finished in",'{:.20f}'.format(end-start), "\n")
-    print("unsorted array: ", arr2[:5])
+        # Do Sequential Merge Sort
+        start = startTime()
+        mergeSort(arr1)
+        end = endTime()
 
-    # Do Parallel Merge Sort
-    p = mp.Process(target=mergeSort(arr2))
-    pStart = startTime()
-    p.start()
-    p.join()
-    pEnd = endTime()
+        # Print Sequential Data
+        print("Sorted Array is: ", arr1[:5])
+        print("Sequential Merge Sort Finished in",'{:.20f}'.format(end-start), "\n")
+        print("unsorted array: ", arr2[:5])
+        sTimeArr.append(end-start)
 
-    # Print Parallel Data
-    print("Sorted array is: ",arr2[:5]) 
-    print("Sort finished in: ",'{:.20f}'.format(pEnd-pStart), end="\n")
+        # Do Parallel Merge Sort
+        p = mp.Process(target=mergeSort(arr2))
+        pStart = startTime()
+        p.start()
+        p.join()
+        pEnd = endTime()
 
-    # Log data
-    logFile(end,start,pEnd,pStart)
+        # Print Parallel Data
+        print("Sorted array is: ",arr2[:5])
+        print("Sort finished in: ",'{:.20f}'.format(pEnd-pStart), end="\n")
+        pTimeArr.append(pEnd-pStart)
 
+        # Print Speedup
+        speedup = (end - start) // (pEnd - pStart)
+
+        # Log data
+        #logFile(end,start,pEnd,pStart)
+        logFile(end,start,pEnd,pStart,speedup)
+
+    sAvg = sumArray(sTimeArr) / len(sTimeArr)
+    print("Average Sequential Sort: ", sAvg, end="\n")
+    pAvg = sumArray(pTimeArr) / len(pTimeArr)
+    print("Average Parallel Sort: ", pAvg, end="\n")
+    avgSpeedup = sAvg // pAvg
+    print("Average Speedup: ", avgSpeedup, end="\n")
