cb2f8a36d4a1e87563a504f373fae6f2d5b05e12
jgearhart13 <79217798+jgearhart13@users.noreply.github.com>
2022-04-06 20:55:10 -0700

    Update mergesort.py

--- mergesort.py
+++ mergesort.py
@@ -2,11 +2,15 @@ from time import perf_counter
 import random
 import multiprocessing as mp
 
-def logFile(time1,time2): #Used to write to a file named merge.log
+def logFile(seqtime1,seqtime2,paratime1,paratime2): #Used to write to a file named merge.log
     f=open("merge.log","a")
-    data = str(time1-time2)
-    f.write("Elapsed time: "+ data + "\n")
+    f.write("Sequential: \n")
+    f.write("Elapsed time: ")
+    f.write('{:.20f}'.format(seqtime1-seqtime2)+ "\n")
+    f.write("Parallel: "+ "\n"+ "Elapsed time: ")
+    f.write('{:.20f}'.format(paratime1-paratime2) +"\n")
     f.close()
+
 
 def startTime():
     start= perf_counter()
@@ -43,12 +47,6 @@ def mergeSort(arr): #takes in an array and performs mergesorts
             j+=1
             k+=1
 
-# used for printing the array, just used for testing probably omit this function
-def printList(arr):
-    for i in range(len(arr)):
-         print(arr[i], end=" ")
-    
-    print()
 
 # Generates a number of element using a gaussian distribution
 def generateNumbers(numElements):
@@ -61,18 +59,30 @@ def generateNumbers(numElements):
 
     return randomArray
 
+def copyArray(arr):
+    equalArray = []
+    for i in range(len(arr)):
+        equalArray.insert(i, arr[i])
+    return equalArray
+
+
 if __name__ == '__main__':
-    arr = [12,11,13,5,6,7]
-    arr = generateNumbers(10) # Generate 10 random numbers in a normal distribution
-    print("Given array is", end="\n")
-    printList(arr)
+    userIn = int(input("Enter the size of the array to be randomly generated ")) 
+    arr1 = generateNumbers(userIn)
+    arr2 = copyArray(arr1)
+    print("Given array is: ", arr1[:5])
     start = startTime()
-    mergeSort(arr)
-    #p = mp.Process(target=mergeSort, args=arr)
-    #p.start()
-    #p.join()
+    mergeSort(arr1)
     end = endTime()
-    print("Sort finished in: ",end-start)
-    print("Sorted array is: ", end="\n")
-    printList(arr)
-    logFile(end,start)
+    print("Sorted Array is: ", arr1[:5])
+    print("Sequential Merge Sort Finished in",'{:.20f}'.format(end-start), "\n")
+    print("unsorted array: ", arr2[:5])
+    p = mp.Process(target=mergeSort(arr2))
+    pStart = startTime()
+    p.start()
+    p.join()
+    pEnd = endTime()
+    print("Sorted array is: ",arr2[:5]) 
+    print("Sort finished in: ",'{:.20f}'.format(pEnd-pStart), end="\n")
+    logFile(end,start,pEnd,pStart)
+
